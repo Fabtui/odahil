71a38a3f2ed8b7ba323cb6268d2f329881bf88d1
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-13 20:02:28 +0100

    add opacity effect on menu

--- app/javascript/components/_show_menu.js
+++ app/javascript/components/_show_menu.js
@@ -1,7 +1,5 @@
 const showMenu = () => {
-  console.log('ok')
   const navTrigger = document.getElementsByClassName('nav-trigger')[0];
-  console.log(navTrigger)
   const body = document.getElementsByTagName('body')[0];
 
   if (navTrigger) {
