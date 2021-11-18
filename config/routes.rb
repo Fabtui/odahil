ab5dff03ecae9d199879aeecb92b2374f691f384
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-18 19:53:19 +0100

    add art section

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'pages/profile'
 
   resources :tattoos
+
+  resources :arts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
