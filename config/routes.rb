eaac16e952c68881f64d90157f950c6e4db3ad2d
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-19 11:23:09 +0100

    add a contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/profile'
   get 'pages/dashboard'
+  match '/contacts', to: 'contacts#new', via: 'get'
+  resources "contacts", only: %i[new create]
 
   resources :tattoos
 
   resources :arts
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
