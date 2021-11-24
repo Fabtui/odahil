4753ab1042a0af9d39fd3071aa930068292f1e07
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-24 17:19:47 +0100

    add a preview of the photo when edit tattoo

--- app/javascript/components/_upload_photo_preview.js
+++ app/javascript/components/_upload_photo_preview.js
@@ -4,13 +4,10 @@ const previewImageOnFileSelect = () => {
     inputs.forEach(input => {
       const id = input.classList.value.slice(-1);
       const previewContainer = document.querySelector(`#photo-preview-${id}`);
-      console.log(previewContainer)
       const displayPreview = (input) => {
-        console.log(input)
         if (input.files && input.files[0]) {
           const reader = new FileReader();
           reader.onload = (event) => {
-            console.log(event.currentTarget.result)
             previewContainer.src = event.currentTarget.result;
           }
           reader.readAsDataURL(input.files[0])
@@ -19,7 +16,6 @@ const previewImageOnFileSelect = () => {
       }
 
       input.addEventListener('change', () => {
-        console.log(previewContainer)
         displayPreview(input);
       })
     })
