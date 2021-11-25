3e91a9c34399f245056b682c9cc42de16f891d4a
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-11-25 10:24:30 +0100

    add message section

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,35 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def index
+    @contacts = Contact.all
+  end
+
   def new
     @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
+    @contact = Contact.new(contact_params)
+    if @contact.save
       flash.now[:notice] = "Message envoyé"
     else
       flash.now[:error] = "Erreur"
       render :new
     end
+    # @contact = Contact.new(params[:contact])
+    # @contact.request = request
+    # if @contact.deliver
+    #   flash.now[:notice] = "Message envoyé"
+    # else
+    #   flash.now[:error] = "Erreur"
+    #   render :new
+    # end
   end
 
   private
 
-  def art_params
-    params.require(:art).permit(:id, :name, :message, :email)
+  def contact_params
+    params.require(:contact).permit(:id, :name, :message, :email)
   end
 end
