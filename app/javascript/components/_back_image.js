89bfb6316b93c4800d9225404744122c6bfc92c6
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-15 16:09:14 +0100

    remove logs

--- app/javascript/components/_back_image.js
+++ app/javascript/components/_back_image.js
@@ -10,18 +10,10 @@ const backImageScroll = () => {
       const bottomWindow = window.pageYOffset + window.innerHeight
       let offSet = -((backImageBottom - bottomWindow)*1.4 / windowHeight) * 100
       if (bottomWindow > backImageBottom) {
-        console.log('start')
-        console.log(bottomWindow);
         if (topWindow > backImageTop) {
-          console.log('stop');
-          console.log(topWindow);
+          return
         } else {
-          console.log(`${Math.floor(offSet)}%`);
-        // console.log(`${offSet}%`)
           backImage.style.backgroundPositionY = (`${offSet}%`);
-          // 0% => windowBottom = backImageBottom || 100% windowTop = backImageTop
-          // we want 0 to 100
-
         }
       }
     })
