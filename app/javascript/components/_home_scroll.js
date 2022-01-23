d88b4aa2b4c9e7194b9ae8fe5b7a6c45d6c3277a
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-23 14:42:28 +0100

    fix home scroll issues

--- app/javascript/components/_home_scroll.js
+++ app/javascript/components/_home_scroll.js
@@ -1,9 +1,10 @@
 const homeScroll = () => {
   const title = document.querySelector('#home-title')
+  const homeBanner = document.querySelector('.home-page-container')
   if (title) {
     title.addEventListener('click', (e) => {
       window.scroll({
-          top: window.innerHeight,
+          top: homeBanner.offsetHeight,
           behavior: "smooth"
         });
     })
