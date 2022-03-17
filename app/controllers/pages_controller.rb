ac32f3d52b1fa9be2e909e454cd98ca0edafa22d
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-03-17 13:16:09 +0100

    add grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home profile]
+  skip_before_action :authenticate_user!, only: %i[home profile shop]
 
   def home
   end
